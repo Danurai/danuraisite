09617e9dfebf1b9486effbf5dc6b52ebe6a77ee3
Danurai <Danurai@users.noreply.github.com>
2021-09-01 16:58:58 +0100

    Add files via upload

--- src/clj/danuraisite/pages/common.clj
+++ src/clj/danuraisite/pages/common.clj
@@ -12,26 +12,20 @@
     [:link {:rel "icon" :href "/img/danuraisite.ico"}]
   ;; jQuery, Bootstrap and Popper
     [:link {
-      :rel "stylesheet"
-      :href "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
-      :integrity "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+      :href "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" 
+      :rel "stylesheet" 
+      :integrity "sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" 
       :crossorigin "anonymous"}]
     [:script {
       :src "https://code.jquery.com/jquery-3.4.1.min.js"
       :integrity "sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
       :crossorigin "anonymous"}]
     [:script {
-      :src "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" 
-      :integrity "sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" 
+      :src "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" 
+      :integrity "sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" 
       :crossorigin "anonymous"}]
-    [:script {
-      :src "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" 
-      :integrity "sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" 
-      :crossorigin "anonymous"}]
+    
   
-  ;; popper tooltip.js
-    [:script {:src "https://cdnjs.cloudflare.com/ajax/libs/tooltip.js/1.3.1/umd/tooltip.min.js" :integrity "sha256-5hYn1dYaPW5VRitzMTQ8UsMvqSPqCiqwtQbT77tyEso=" :crossorigin="anonymous"}]
-    ;[:script {:src "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" :integrity "sha256-WHwIASWxNdKakx7TceUP/BqWQYMcEIfeLNdFMoFfRWA=" :crossorigin "anonymous"}]
   ;; Bootstrap Select
     [:link {:rel "stylesheet" :href "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.8/css/bootstrap-select.css" :integrity "sha256-OejstTtgpWqwtX/gwUbICEQz8wbdVWpVrCwqZ29apg4=" :crossorigin "anonymous"}]
     [:script {:src "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.8/js/bootstrap-select.js" :integrity "sha256-/X1l5JQfBqlJ1nW6V8EhZJsnDycL6ocQDWd531nF2EI=" :crossorigin "anonymous"}]
@@ -43,64 +37,54 @@
     ;; (h/include-css "/css/danuraisite.css?v=1")
     ])
     
-(defn- navlink [ uri linkuri linkname ]
-  [:li.nav-item {:class (if (= uri linkuri) "active")}
-    [:a.nav-link {:href linkuri} linkname]])
   
-
-(defn navbar [req]
-  [:nav.navbar.navbar-expand-lg.navbar-dark.bg-dark {:style "z-index: 1021;"}
-    [:div.container-fluid
-    ;; Home Brand with Icon
-      [:a.navbar-brand.mb-0.h1 {:href "/"}
-        "Home"]
-    ;; Collapse Button for smaller viewports
-      [:button.navbar-toggler {:type "button" :data-toggle "collapse" :data-target "#navbarSupportedContent" 
-                            :aria-controls "navbarSupportedContent" :aria-label "Toggle Navigation" :aria-expanded "false"}
-        [:span.navbar-toggler-icon]]
-    ;; Collapsable Content
-      [:div#navbarSupportedContent.collapse.navbar-collapse
-    ;; List of Links
-        [:ul.navbar-nav.mr-auto
-          [:li.nav-item.dropdown
-            [:a.nav-link.dropdown-toggle {:href "#" :role "dropdown" :data-toggle "dropdown"} "LUGS"]
-            [:div.dropdown-menu
-              ;[:a.dropdown-item {:href "/lu"} "Sheets"]
-              [:a.dropdown-item {:href "/lu/party"} "Party"]
-              [:a.dropdown-item {:href "/lu/icons"} "Icons"]
-              [:a.dropdown-item {:href "/lu/api"} "API"]]]
-          [:li.nav-item.dropdown
-            [:a.nav-link.dropdown-toggle {:href "#" :role "dropdown" :data-toggle "dropdown"} "Colours"]
-            [:div.dropdown-menu
-              [:a.dropdown-item {:href "/colours/hsl"} "HSL Demo"]
-              [:a.dropdown-item {:href "/colours/citadel"} "Citadel Colour Range"]]]
-          (navlink (:uri req) "/don" "DoN Sheets")
-          [:li.nav-item.dropdown
-            [:a.nav-link.dropdown-toggle {:href "#" :role "dropdown" :data-toggle "dropdown"} "Netrunner"]
-            [:div.dropdown-menu
-              [:a.dropdown-item {:href "/netrunner/mwl"} "MWL Checker"]
-              [:a.dropdown-item {:href "/netrunner/nrf"} "Virtual Folder"]]]
-          (navlink (:uri req) "/scores" "Scores")
-          (navlink (:uri req) "/killteam" "Kill Team")]
-    ;; Login Icon
-          [:span.nav-item.dropdown
-            [:a#userDropdown.nav-link.dropdown-toggle.text-white {:href="#" :role "button" :data-toggle "dropdown" :aria-haspopup "true" :aria-expanded "false"}
-              [:i.fas.fa-user]]
-              (if-let [identity (friend/identity req)]
-                [:div.dropdown-menu {:aria-labelledby "userDropdown"}
-                  (if (friend/authorized? #{::db/admin} (friend/identity req))
-                    [:a.dropdown-item {:href "/admin"} "Admin Console"])
-                  [:a.dropdown-item {:href "/logout"} "Logout"]]
-                [:div.dropdown-menu {:aria-labelledby "userDropdown"}
-                  [:a.dropdown-item {:href (str (if (= "/" (:uri req)) "" (:uri req)) "/login")} "Login"]])]]]])      
-
+    (defn- navlink [ uri linkuri linkname ]
+      [:li.nav-item {:class (if (= uri linkuri) "active")}
+        [:a.nav-link {:href linkuri} linkname]])
+      
     
-
-                  
-;(defn show-alert []
-;  (let [type (-> @model/alert :type)
-;        msg  (-> @model/alert :message)]
-;    (when (some? type)
-;      (reset! model/alert {})
-;      [:div.alert.alert-dismissible.fade.show {:class type :role "alert"} msg
-;        [:button.close {:type"button" :data-dismiss "alert" :aria-label "Close"} [:span {:aria-hidden "true"} "&#10799;"]]])))
+    (defn navbar [req]
+      [:nav.navbar.navbar-expand-lg.navbar-dark.bg-dark {:style "z-index: 1021;"}
+        [:div.container-fluid
+        ;; Home Brand with Icon
+          [:a.navbar-brand.mb-0.h1 {:href "/"}
+            "Home"]
+        ;; Collapse Button for smaller viewports
+          [:button.navbar-toggler {:type "button" :data-toggle "collapse" :data-target "#navbarSupportedContent" 
+                                :aria-controls "navbarSupportedContent" :aria-label "Toggle Navigation" :aria-expanded "false"}
+            [:span.navbar-toggler-icon]]
+        ;; Collapsable Content
+          [:div#navbarSupportedContent.collapse.navbar-collapse
+        ;; List of Links
+            [:ul.navbar-nav
+              [:li.nav-item.dropdown
+                [:a.nav-link.dropdown-toggle {:href "#" :role "button" :data-bs-toggle "dropdown"} "LUGS"]
+                [:div.dropdown-menu
+                  ;[:a.dropdown-item {:href "/lu"} "Sheets"]
+                  [:a.dropdown-item {:href "/lu/party"} "Party"]
+                  [:a.dropdown-item {:href "/lu/icons"} "Icons"]
+                  [:a.dropdown-item {:href "/lu/api"} "API"]]]
+              [:li.nav-item.dropdown
+                [:a.nav-link.dropdown-toggle {:href "#" :role "button" :data-bs-toggle "dropdown"} "Colours"]
+                [:div.dropdown-menu
+                  [:a.dropdown-item {:href "/colours/hsl"} "HSL Demo"]
+                  [:a.dropdown-item {:href "/colours/citadel"} "Citadel Colour Range"]]]
+              (navlink (:uri req) "/don" "DoN Sheets")
+              [:li.nav-item.dropdown
+                [:a.nav-link.dropdown-toggle {:href "#" :role "button" :data-bs-toggle "dropdown"} "Netrunner"]
+                [:div.dropdown-menu
+                  [:a.dropdown-item {:href "/netrunner/mwl"} "MWL Checker"]
+                  [:a.dropdown-item {:href "/netrunner/nrf"} "Virtual Folder"]]]
+              (navlink (:uri req) "/scores" "Scores")
+              (navlink (:uri req) "/killteam" "Kill Team")]
+        ;; Login Icon
+            [:span.nav-item.dropdown.ms-auto
+              [:a#userDropdown.nav-link.dropdown-toggle.text-white {:href="#" :role "button" :data-bs-toggle "dropdown"} [:i.fas.fa-user]]
+                  (if-let [identity (friend/identity req)]
+                    [:div.dropdown-menu {:aria-labelledby "userDropdown"}
+                      (if (friend/authorized? #{::db/admin} (friend/identity req))
+                        [:a.dropdown-item {:href "/admin"} "Admin Console"])
+                      [:a.dropdown-item {:href "/logout"} "Logout"]]
+                    [:div.dropdown-menu {:aria-labelledby "userDropdown"}
+                      [:a.dropdown-item {:href (str (if (= "/" (:uri req)) "" (:uri req)) "/login")} "Login"]])]]]])      
+    
