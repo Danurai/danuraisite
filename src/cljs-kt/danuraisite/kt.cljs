4c1ea78dc19c3f45f9b2b5f768dd39e63d6ecd56
Danurai <bikerdan13@yahoo.co.uk>
2021-09-06 19:07:19 +0100

    kt fixes

--- src/cljs-kt/danuraisite/kt.cljs
+++ src/cljs-kt/danuraisite/kt.cljs
@@ -22,7 +22,6 @@
 (def numbers ["zero" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "ten"])
 
 (defn- markup [& args]
-    (prn (apply str args) )
     [:span {:dangerouslySetInnerHTML {:__html
         (-> (apply str args) 
             (clojure.string/replace #"\[\[" "<span class=\"keyword\">")
