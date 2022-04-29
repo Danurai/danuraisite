14dfa8ae5c5afd5eba933765627fe167f744424c
Danurai <Danurai@users.noreply.github.com>
2022-04-29 17:05:04 +0100

    Add files via upload

--- src/clj/danuraisite/pages.clj
+++ src/clj/danuraisite/pages.clj
@@ -13,41 +13,6 @@
 (load "pages/admin")
 (load "pages/legendsuntold")
 (load "pages/colour")
-     
-(defn paintranges [ req ]
-  (h/html5
-    pretty-head
-    [:body
-      (navbar req)
-      [:div#filterModal.modal {:tabindex -1}
-        [:div.modal-dialog
-          [:div.modal-content 
-            [:div.modal-header
-              [:h4.modal-title "Filters"]
-              [:button.btn-close {:type "button" :data-bs-dismiss "modal"} ]]
-            [:div.modal-body
-              [:div.d-flex
-                [:div.flex-fill.me-1
-                  [:h5 "Brand"]
-                  [:select#brandmulti.form-control {:multiple true :size 10}]]
-                [:div.flex-fill 
-                  [:h5 "Range"]
-                  [:select#rangemulti.form-control {:multiple true :size 10}]]]]
-            [:div.modal-footer
-              [:button.btn.btn-primary {:type "button" :data-bs-dismiss "modal"} "OK"]]]]]
-      [:div.container-fluid.my-3
-        [:div#comparison.d-flex]
-        [:div#matches.d-flex.mb-2]
-        [:div
-          [:table#colourtable]]]
-      (h/include-css "//cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css")
-      (h/include-js  "//cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js")
-      ;<link rel="stylesheet" type="text/css" href="DataTables/datatables.min.css"/>
-      ;<script type="text/javascript" src="DataTables/datatables.min.js"></script>
-      (h/include-js  "/js/paintchart2.js")
-      (h/include-css "/css/colour.css")
-      ]))
-
 (load "pages/deadofnight")
 (load "pages/netrunner")
 (load "pages/kt2")
@@ -55,12 +20,45 @@
 
 (def custom-formatter (tf/formatter "dd/MMM/yyyy"))
 
+(defn homepage [ req ]
+  (h/html5 
+    pretty-head
+    [:body
+      (navbar req)
+      [:div.container.my-3
+        [:div.row
+          [:div.col
+            [:h5 "Legends Untold: The Great Sewer (LUGS)"]
+            [:div.mb-2 "Character / Party Builder and Icon Reference"]
+            [:h5 "Colours"]
+            [:div.mb-2 [:b "H"] "ue " [:b "S"] "aturation and " [:b "L"] "uminance demo and Paint comparison chart"]
+            [:h5 "Dead of Night (DoN)"]
+            [:div.mb-2 "Victim roll..."]
+            [:h5 "Netrunner"]
+            [:div.mb-2 "Rotation Checker, Most Wanted List (MWL) and Folder planner"]
+            [:h5 "Scores"]
+            [:div.mb-2 "Spirit Island score log"]
+            [:h5 "Kill Team"]
+            [:div.mb-2 "Sample Compendium Reference and Spec Ops Dataslates"]
+            [:h5 "Bardsung"]
+            [:div.mb-2 "Marching Order Randomiser"]
+          ]]]]))
+      
 (defn scorehome [ req ]
   (h/html5 
     pretty-head
     [:body
       (navbar req)
-      [:div#app]
+      [:div#mapmodal.modal.fade {:tabindex -1}
+        [:div.modal-dialog.modal-lg
+          [:div.modal-content
+            [:div.modal-header
+              [:h5 "Scenario Map"]
+              [:button.btn-close {:type "button" :data-bs-dismiss "modal"}]]
+            [:div.modal-body
+              [:canvas#drawing.border.mx-auto {:width "700px" :height "450px"}]]]]]
+      [:div#app
+        [:div.ms-3 "loading..."]]
       [:div.container-fluid.mb-3
         [:div.row 
           [:table.table.table-hover
@@ -95,44 +93,5 @@
                   [:td.text-center (:blight gm)]
                   [:td.text-center (:score gm)]
                 ]
-              )]]]
-        [:div.row 
-          [:canvas#drawing.border {:width "800px" :height "450px"}]]]
+              )]]]]
       (h/include-js "/js/compiled/scoresapp.js")]))
-
-(defn homepage [ req ]
-  (h/html5 
-    pretty-head
-    [:body
-      (navbar req)
-      [:div.container.my-3
-        [:div.row
-          [:div.col
-            [:h5 "Legends Untold"]
-            [:div.mb-2 "Character Builder and Icon Reference"]
-            [:h5 "Age of Sigmar: Champions (AoS:C)"]
-            [:div.mb-2 "Card list, tooltip demo and API links"]
-            [:h5 "Colours"]
-            [:div.mb-2 [:b "H"] "ue " [:b "S"] "aturation and " [:b "L"] "uminance Demo and Citadel Paint comparison chart"]
-            [:h5 "Dead of Night (DoN)"]
-            [:div.mb-2 "Victim roll..."]
-            [:h5 "Netrunner"]
-            [:div.mb-2 "Rotation Checker and Folder planner"]
-          ]]]]))
-      
-(defn painttbl [ req ]
-	(let [cols (-> "private/paintlist.json" io/resource slurp (json/read-str :key-fn keyword))]
-		(h/html5
-			pretty-head
-			[:div.container.my-3
-				[:table#tbl.table
-					[:thead [:tr [:th "Name"][:th "Range"][:th "OldGW"][:th "Hex"]]]
-					[:tbody
-						(for [clr cols]
-							[:tr [:td (:name clr)][:td (:range clr)][:td (:oldgw clr)][:td (:hex clr)]])]
-				]]
-			[:script "$(document).ready(function(){$('#tbl').DataTable({paging: false})});"]
-			(h/include-css "//cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css")
-			(h/include-js "//cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"))))
-
-
